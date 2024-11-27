682acdc182cc518ad6ea482184e30c563afa90f5
Elielvitor45 <Elielvitor2020tb@gmail.com>
2024-11-26 23:18:09 -0300

    fix: Init chatbot

---
+++ ChatBot_/AnalisadorLexico/Analisador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChatBot_.AnalisadorLexico
+{
+    public class Analisador
+    {
+
+
+        public string CheckSimilarity(String character) {
+
+
+
+
+            return string.Empty;
+        }
+
+        public bool ValidateCharacter(String input) {
+            //Regex rgx = new Regex(input);
+            String alphabet = @"(?i)^[a-zA-Z0-9 .,;:?!""'()\[\]{}<>¨+=*/%çáéíóúâêôãõàèìòùäëïöüñ$&@#^~`´|\\_\t\n\r-]*$";
+            if (Regex.IsMatch(input,alphabet))
+            {
+                return true;
+            }
+            else
+            {
+                List<char> invalidChars = new List<char>();
+                foreach (char c in input) {
+                    if (!Regex.IsMatch(c.ToString(),alphabet)) {
+                        if (!invalidChars.Any(x => x.Equals(c))) { 
+                            invalidChars.Add(c);
+                        }
+                    }
+                }
+                if (invalidChars.Count > 0) {
+                    string InvalidCharacter = String.Join("",invalidChars.Select(c => $"[{c}]"));
+                    Console.WriteLine($"Error: Invalid Character: {InvalidCharacter}");
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ ChatBot_/Program.cs
@@ -0,0 +1,16 @@
+﻿using ChatBot_.AnalisadorLexico;
+
+namespace ChatBot_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a frase a ser validada. ");
+            String input = Console.ReadLine();
+            Analisador analisador = new Analisador();
+            analisador.ValidateCharacter(input);
+
+        }
+    }
+}
